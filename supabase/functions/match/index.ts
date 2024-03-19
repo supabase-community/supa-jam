43b345a4c6871cc978a34c0a736a314ee2369e7b
dshukertjr <dshukertjr@gmail.com>
2024-03-19 14:40:08 +0700

    adjust the code to slack web client

--- supabase/functions/match/index.ts
+++ supabase/functions/match/index.ts
@@ -5,13 +5,13 @@ const slackBotToken = Deno.env.get("SLACK_TOKEN") ?? "";
 const botClient = new WebClient(slackBotToken);
 
 // Pulls the users, creates matches, and inserts it into the database
+import { createClient } from "npm:@supabase/supabase-js@2";
 
-interface SlackMember {
-  id: string;
-  name: string;
-  real_name: string;
+interface Match {
+  users: string[];
 }
 
+// Pulls the users, creates matches, and inserts it into the database
 Deno.serve(async (req) => {
   const userListResponse = await botClient.users.list({});
 
@@ -21,11 +21,33 @@ Deno.serve(async (req) => {
     });
   }
 
-  const userList: SlackMember[] = userListResponse.members;
+  const userList = userListResponse.members!;
 
   // TODO: match the users
 
+  const shuffledUserList = userList
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+  const matches: Match[] = [];
+
+  for (const slackUser of shuffledUserList) {
+    const isEmptyMatch = matches[matches.length - 1].users.length == 0;
+    if (isEmptyMatch) {
+      matches[matches.length - 1].users.push(slackUser.id!);
+    } else {
+      matches.push({ users: [slackUser.id!] });
+    }
+  }
+
   // TODO: insert the maches into the database
+  const supabase = createClient(
+    Deno.env.get("SUPABASE_URL")!,
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
+  );
+
+  supabase.from("matches").insert(matches);
 
   return Response.json({ members: userList, total: userList.length });
 });
