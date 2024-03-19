76f622df04dc95a9bd856a55e8b22bdfc31a3b26
thorwebdev <thor@supabase.io>
2024-03-19 12:59:46 +0700

    feat: add slack web client.

--- supabase/functions/match/index.ts
+++ supabase/functions/match/index.ts
@@ -1,13 +1,16 @@
+import { WebClient } from "npm:@slack/web-api";
+
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+const slackBotToken = Deno.env.get("SLACK_TOKEN") ?? "";
+const botClient = new WebClient(slackBotToken);
+
 // Pulls the users, creates matches, and inserts it into the database
 
 Deno.serve(async (req) => {
-  if (req.method !== 'GET') {
-    return new Response('Not Get request', {
-      headers: { 'Content-Type': 'application/json' },
-    })
-  }
+  const users = await botClient.users.list({});
 
-  return new Response(JSON.stringify({}), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-})
+  return Response.json({
+    members: users.members,
+    total: users.members?.length,
+  });
+});
