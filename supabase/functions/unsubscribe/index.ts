602973883729759e055db288267ad99000a850eb
thorwebdev <thor@supabase.io>
2024-03-21 12:38:30 +0700

    chore: fix unsubscribe.

--- supabase/functions/unsubscribe/index.ts
+++ supabase/functions/unsubscribe/index.ts
@@ -1,37 +1,35 @@
+import express from "npm:express@4.18.2";
 import { createClient } from "npm:@supabase/supabase-js@2";
+import { Database } from "../_shared/database.types.ts";
 
-// Unsubscribes a user from the donut bot
-Deno.serve(async (req) => {
-  if (req.method !== "POST") {
-    return new Response(
-      "Unauthorized",
-      { status: 400, headers: { "Content-Type": "application/json" } },
-    );
-  }
+const app = express();
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
+const port = 3000;
 
-  // TODO: check the shape of the incoming request from slack
-  const { user } = await req.json();
-
+app.post("/unsubscribe", async (req: any, res: any) => {
+  const { payload } = req.body;
+  const { user } = JSON.parse(payload);
   const slackId = user.id;
 
-  const supabase = createClient(
+  const supabase = createClient<Database>(
     Deno.env.get("SUPABASE_URL")!,
     Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
   );
 
   const { error } = await supabase.from("members").update({
     is_subscribed: false,
-  }).eq("slack_id", slackId);
+  }).eq("id", slackId);
 
   if (error) {
-    return new Response(
-      JSON.stringify({ error: error.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } },
-    );
+    console.log(error.message);
+    return res.send(error.message);
   }
 
-  return new Response(
-    "ok",
-    { headers: { "Content-Type": "application/json" } },
-  );
+  // TODO: Send unsubscribe confirmation
+
+  res.send(`ok`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
